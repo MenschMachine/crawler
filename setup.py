ed1b287879900c8b72556b9ecdeedc7a0836b49e
Michael Lahr <michael.lahr@gmail.com>
2025-05-05 14:49:31 +0800

    ci: update release workflow and package configuration

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="crawler",
-    version="0.1.0",
-    author="Joe",
-    author_email="joseribeiro1017@gmail.com",
+    name="usual-suspects-crawler",  # Consider a more unique name like "web-md-crawler" to avoid conflicts on PyPI
+    version="0.1.0",  # This will be managed by your release process
+    author="Michael Lahr",
+    author_email="michael.lahr@gmail.com",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "networkx",
+        "html2text",
+        "matplotlib",  # For visualization
+        "numpy",
+    ],
     entry_points={
         "console_scripts": [
             "crawler=crawler:main",
         ],
     },
-    description="A simple crawler.",
+    description="A simple web crawler that converts HTML content to Markdown",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    url="https://github.com/MenschMachine/crawler",  # Add your repository URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
